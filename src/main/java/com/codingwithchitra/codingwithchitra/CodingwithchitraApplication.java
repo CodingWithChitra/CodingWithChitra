965e164a1571821a71698648f57dfa769a982456
CodingWithChitra <cancoding@gmail.com>
2024-06-15 21:37:07 +0700

    my first commit

---
+++ src/main/java/com/codingwithchitra/codingwithchitra/CodingwithchitraApplication.java
@@ -0,0 +1,13 @@
+package com.codingwithchitra.codingwithchitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingwithchitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingwithchitraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingwithchitra/codingwithchitra/CodingwithchitraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingwithchitra.codingwithchitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingwithchitraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
